398805ea39cc6faffa03c64ac81c8296bce3938e
Juan Morocho <jc23ace@gmail.com>
2017-06-04 22:06:29 -0500

    Backend agregado persistencia

---
+++ Source Code Backend/ProyectoFinal/src/main/java/com/uda/model/Usuario.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uda.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Anticlutch
+ */
+@Entity
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre_usuario;
+    private String password;
+    private String nombre_ape;
+    private String email;
+    private String telefono;
+    private String tipo_usuario;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "java.proyectofinal.model.Usuario[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the nombre_usuario
+     */
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    /**
+     * @param nombre_usuario the nombre_usuario to set
+     */
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the nombre_ape
+     */
+    public String getNombre_ape() {
+        return nombre_ape;
+    }
+
+    /**
+     * @param nombre_ape the nombre_ape to set
+     */
+    public void setNombre_ape(String nombre_ape) {
+        this.nombre_ape = nombre_ape;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the tipo_usuario
+     */
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    /**
+     * @param tipo_usuario the tipo_usuario to set
+     */
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+    
+}
---
+++ Source Code Backend/ProyectoFinal/target/generated-sources/annotations/com/uda/model/Usuario_.java
@@ -0,0 +1,20 @@
+package com.uda.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Usuario.class)
+public abstract class Usuario_ {
+
+	public static volatile SingularAttribute<Usuario, String> nombre_ape;
+	public static volatile SingularAttribute<Usuario, String> password;
+	public static volatile SingularAttribute<Usuario, Long> id;
+	public static volatile SingularAttribute<Usuario, String> telefono;
+	public static volatile SingularAttribute<Usuario, String> nombre_usuario;
+	public static volatile SingularAttribute<Usuario, String> tipo_usuario;
+	public static volatile SingularAttribute<Usuario, String> email;
+
+}
+
